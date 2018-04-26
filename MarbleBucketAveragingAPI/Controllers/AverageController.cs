40f1a2b838b881bae73acfeb5de1afa0678ca01f
Joaquin Paniagua <joato_pgl91@hotmail.com>
2018-04-26 14:18:47 -0700

    Initial commit

---
+++ MarbleBucketAveragingAPI/Controllers/AverageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MarbleBucketAveragingAPI.Controllers
+{
+    public class AverageController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult PostValues([FromBody] double[] values)
+        {
+            if(values == null)
+            {
+                return BadRequest();
+            }
+
+            Console.Write("Received the following values: {0}. ", String.Join(", ", values));
+
+            double result = values.Average();
+
+            Console.WriteLine("Returning the following result: {0}.", result);
+
+            return Ok(result);
+
+        }
+
+    }
+}
---
+++ MarbleBucketAveragingAPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarbleBucketAveragingAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseUrl))
+            {
+                Console.WriteLine("Started API server. Listening on {0}", baseUrl );
+                Console.ReadLine();
+            }
+        }
+    }
+}
